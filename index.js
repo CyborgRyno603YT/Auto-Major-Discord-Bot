e6830da9a94d74a34de5e6911432eb2bc6cb16ac
CyborgRyno603YT <ryno10292005@gmail.com>
2022-07-28 15:16:28 -0500

    First steps
    The bot has basic commands and replies according to said commands.

---
+++ index.js
@@ -0,0 +1,29 @@
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Replying to slash commands
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nTime created: ${interaction.guild.createdAt}\nVerification level: ${interaction.guild.verificationLevel}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+});
+
+// Login to Discord with bot's token
+client.login(token);
