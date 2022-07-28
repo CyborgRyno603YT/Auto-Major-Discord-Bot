726bfb3820f43c651148f33e6f0c72f0a859a62f
CyborgRyno603YT <ryno10292005@gmail.com>
2022-07-28 16:14:40 -0500

    Organized commands
    Uses a file based command handling instead of an if/then tree

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
-// Require the necessary discord.js classes
-const { Client, GatewayIntentBits } = require('discord.js');
+// Initial start up code
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
+// Starts a new client
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once)
+// Defines where to find the commands and what file to run for each slash command
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+// This code runs once at the start of the program and prints 'Ready' to the console when the bot is ready to recieve commands
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-// Replying to slash commands
+// This defines what to do when the bot recieves a command
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
-	const { commandName } = interaction;
+	const command = client.commands.get(interaction.commandName);
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nTime created: ${interaction.guild.createdAt}\nVerification level: ${interaction.guild.verificationLevel}`);
-	} else if (commandName === 'user') {
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
-// Login to Discord with bot's token
+// Login to Discord with the bot token
 client.login(token);
